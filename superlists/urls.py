41a331cfed9c78b0aa53a62147ad6e53db4de3b0
Tuomas Tielinen <tuomas.tielinen>
2015-04-08 20:38:15 +0300

    Fixed formatting

--- superlists/urls.py
+++ superlists/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
                        url(r'^$', 'lists.views.home_page', name='home'),
-url(r'^lists/', include('lists.urls')),
+                       url(r'^lists/', include('lists.urls')),
                        # url(r'^admin/', include(admin.site.urls)),
                        )

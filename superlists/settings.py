a3bb8e04ce7c78718b0b948f07932448397b7a88
Tuomas Tielinen <tuomas.tielinen>
2015-04-20 20:18:33 +0300

    Moved static files and added app for accounts

--- superlists/settings.py
+++ superlists/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    #'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -83,3 +83,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'superlists', 'static'),
+)
